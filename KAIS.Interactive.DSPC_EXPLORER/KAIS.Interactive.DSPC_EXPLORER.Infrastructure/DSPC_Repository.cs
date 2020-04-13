17346abd6628eed2350f8467f1173493a30ca437
Abdul Saliu <abdulofficial@icloud.com>
2020-04-13 02:44:29 +0100

    fix folder name

---
+++ KAIS.Interactive.DSPC_EXPLORER/KAIS.Interactive.DSPC_EXPLORER.Infrastructure/DSPC_Repository.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Threading.Tasks;
+using KAIS.Interactive.DSPC_EXPLORER.Infrastructure.Interface;
+using KAIS.Interactive.DSPC_EXPLORER.Infrastructure.Model;
+
+namespace KAIS.Interactive.DSPC_EXPLORER.Infrastructure
+{
+    public class DSPC_Repository : IDSPC_Repository
+    {
+        public async Task AddNewUser(User user)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ KAIS.Interactive.DSPC_EXPLORER/KAIS.Interactive.DSPC_EXPLORER.Infrastructure/Interface/IDSPC_Repository.cs
@@ -0,0 +1,12 @@
+﻿
+using KAIS.Interactive.DSPC_EXPLORER.Infrastructure.Model;
+using System.Threading.Tasks;
+
+namespace KAIS.Interactive.DSPC_EXPLORER.Infrastructure.Interface
+{
+    public interface IDSPC_Repository
+    {
+
+        Task AddNewUser(User user);
+    }
+}
