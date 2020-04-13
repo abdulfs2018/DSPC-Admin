1a08b316ff60a14a897aef16f3a18ebeec41a77a
Abdul Saliu <abdulofficial@icloud.com>
2020-04-13 05:14:48 +0100

    repository changes

--- KAIS.Interactive.DSPC_EXPLORER/KAIS.Interactive.DSPC_EXPLORER.Infrastructure/DSPC_Repository.cs
+++ KAIS.Interactive.DSPC_EXPLORER/KAIS.Interactive.DSPC_EXPLORER.Infrastructure/DSPC_Repository.cs
@@ -1,9 +1,30 @@
 ﻿
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using KAIS.Interactive.DSPC_EXPLORER.Infrastructure.Interface;
+using KAIS.Interactive.DSPC_EXPLORER.Infrastructure.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace KAIS.Interactive.DSPC_EXPLORER.Infrastructure
 {
-    public class DSPC_Repository: IDSPC_Repository
+    public class DSPC_Repository : IDSPC_Repository
     {
+        private DSPC_ExplorerDbContext _dbContext;
+
+        public DSPC_Repository(DSPC_ExplorerDbContext context)
+        {
+            _dbContext = context;
+        }
+        public async Task<bool> AddNewCompanyRecord(Registrar registrar)
+        {
+            //var existingRegistrars = _dbContext.Registrars.AllAsync(); ;
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<List<Registrar>> GetCompanyRecords()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- KAIS.Interactive.DSPC_EXPLORER/KAIS.Interactive.DSPC_EXPLORER.Infrastructure/Interface/IDSPC_Repository.cs
+++ KAIS.Interactive.DSPC_EXPLORER/KAIS.Interactive.DSPC_EXPLORER.Infrastructure/Interface/IDSPC_Repository.cs
@@ -1,7 +1,14 @@
 ﻿
+using KAIS.Interactive.DSPC_EXPLORER.Infrastructure.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace KAIS.Interactive.DSPC_EXPLORER.Infrastructure.Interface
 {
     public interface IDSPC_Repository
     {
+        Task<bool> AddNewCompanyRecord(Registrar registrar);
+        Task<List<Registrar>> GetCompanyRecords();
+        
     }
 }
