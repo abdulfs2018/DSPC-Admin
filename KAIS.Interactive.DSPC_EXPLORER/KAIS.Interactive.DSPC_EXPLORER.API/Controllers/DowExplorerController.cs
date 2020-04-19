99ba79129b174c61c76229f51db4558d50903381
Kieron Peters <kieronpeters1@gmail.com>
2020-04-19 03:58:52 +0100

    reorded api calls and fixed some code spacing up

--- KAIS.Interactive.DSPC_EXPLORER/KAIS.Interactive.DSPC_EXPLORER.API/Controllers/DowExplorerController.cs
+++ KAIS.Interactive.DSPC_EXPLORER/KAIS.Interactive.DSPC_EXPLORER.API/Controllers/DowExplorerController.cs
@@ -63,6 +63,52 @@ namespace KAIS.Interactive.DSPC_EXPLORER.API.Controllers
             }
         }
 
+        [HttpGet("kais/api/dspc_explorer/getsectionbycode")]
+        public async Task<IActionResult> GetSectionByCode(string code)
+        {
+            try
+            {
+
+                var section = await _repository.GetSectionByCode(code);
+
+                if (section != null)
+                {
+                    return Ok(section);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("kais/api/dspc_explorer/getregistrarbyreferencecode")]
+        public async Task<IActionResult> GetRegistrarByReferenceCode(string code)
+        {
+            try
+            {
+
+                var registrar = await _repository.GetRegistrarByReferenceCode(code);
+
+                if (registrar != null)
+                {
+                    return Ok(registrar);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
         [HttpPost("kais/api/dspc_explorer/addnewgraveowner")]
         public async Task<IActionResult> AddNewGraveOwner(string subId, string jKIndex, string graveReferenceCode, 
             string section, int graveRow, int graveDepth, string graveSize, string graveLocation, bool graveHeadStone, string graveOwnerName, string graveOwnerAddress, string remarks)
@@ -119,7 +165,6 @@ namespace KAIS.Interactive.DSPC_EXPLORER.API.Controllers
             try
             {
 
-
                 Section newSection = new Section
                 {
                     Code = code,
@@ -132,29 +177,6 @@ namespace KAIS.Interactive.DSPC_EXPLORER.API.Controllers
                 if (inserted)
                 {
                     return Ok(true);
-                }
-                else
-                {
-                    return NotFound();
-                }
-            }
-            catch (Exception)
-            {
-                return BadRequest();
-            }
-        }
-
-        [HttpGet("kais/api/dspc_explorer/getsectionbycode")]
-        public async Task<IActionResult> GetSectionByCode(string code)
-        {
-            try
-            {
-
-                var section = await _repository.GetSectionByCode(code);
-
-                if (section != null)
-                {
-                    return Ok(section);
                 }
                 else
                 {
@@ -218,30 +240,6 @@ namespace KAIS.Interactive.DSPC_EXPLORER.API.Controllers
                 return BadRequest();
             }
         }
-
-        [HttpGet("kais/api/dspc_explorer/getregistrarbyreferencecode")]
-        public async Task<IActionResult> GetRegistrarByReferenceCode(string code)
-        {
-            try
-            {
-
-                var registrar = await _repository.GetRegistrarByReferenceCode(code);
-
-                if (registrar != null)
-                {
-                    return Ok(registrar);
-                }
-                else
-                {
-                    return NotFound();
-                }
-            }
-            catch (Exception)
-            {
-                return BadRequest();
-            }
-        }
-
 
         #region repository
         private readonly IDSPC_Repository _repository;
