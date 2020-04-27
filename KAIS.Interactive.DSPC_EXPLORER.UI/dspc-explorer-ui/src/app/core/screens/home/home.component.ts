a38df06f8ecff087a581d83d1797599634dd829e
Kieron Peters <kieronpeters1@gmail.com>
2020-04-27 22:00:23 +0100

    added new buttons to styling and new form with styling

--- KAIS.Interactive.DSPC_EXPLORER.UI/dspc-explorer-ui/src/app/core/screens/home/home.component.ts
+++ KAIS.Interactive.DSPC_EXPLORER.UI/dspc-explorer-ui/src/app/core/screens/home/home.component.ts
@@ -5,11 +5,33 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  readonly style = "style";
+
+  counter(i: number) {
+    return new Array(i);
+  }
+
+  toggleDivs(selectorA, selectorB) {
+    let containerA: HTMLElement = document.getElementById(selectorA);
+    let containerB: HTMLElement = document.getElementById(selectorB);
+
+    console.log(containerA.getAttribute(this.style));
+
+    if (containerA.getAttribute(this.style) === "display:block;") {
+      containerA.setAttribute(this.style, "display:none;");
+      containerB.setAttribute(this.style, "display:block;");
+    } else {
+      containerA.setAttribute(this.style, "display:block;");
+      containerB.setAttribute(this.style, "display:none;");
+    }
   }
 
 }
