01a37e88b074b250f0642b36906716549d1b2440
Abdul Saliu <abdulofficial@icloud.com>
2020-05-06 21:16:14 +0100

    screens moved out of core.. api wrapper service

--- KAIS.Interactive.DSPC_EXPLORER.UI/dspc-explorer-ui/src/app/app-routing.module.ts
+++ KAIS.Interactive.DSPC_EXPLORER.UI/dspc-explorer-ui/src/app/app-routing.module.ts
@@ -1,19 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent} from './core\\screens/home/home.component';
-import { AboutUsComponent} from './core\\screens/about-us/about-us.component';
-import { UserLoginComponent} from './core\\screens/user-login/user-login.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./screens/home/home.component";
+import { AboutUsComponent } from "./screens/about-us/about-us.component";
+import { UserLoginComponent } from "./screens/user-login/user-login.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutUsComponent },
-  { path: 'login', component: UserLoginComponent }, 
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomeComponent },
+  { path: "about", component: AboutUsComponent },
+  { path: "login", component: UserLoginComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- KAIS.Interactive.DSPC_EXPLORER.UI/dspc-explorer-ui/src/app/app.module.ts
+++ KAIS.Interactive.DSPC_EXPLORER.UI/dspc-explorer-ui/src/app/app.module.ts
@@ -1,26 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './core/screens/home/home.component';
-import { AboutUsComponent } from './core/screens/about-us/about-us.component';
-import { UserLoginComponent } from './core/screens/user-login/user-login.component';
-import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./screens/home/home.component";
+import { AboutUsComponent } from "./screens/about-us/about-us.component";
+import { UserLoginComponent } from "./screens/user-login/user-login.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutUsComponent,
-    UserLoginComponent
+    UserLoginComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
