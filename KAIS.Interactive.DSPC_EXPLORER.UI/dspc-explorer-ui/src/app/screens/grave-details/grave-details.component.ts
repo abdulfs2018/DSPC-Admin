5b6d8bb32106425dab1e857b906d1b17a8ff38c2
Kieron Peters <kieronpeters1@gmail.com>
2020-05-17 23:43:42 +0100

    TASK17 added new component and used buttons to navigate to it

---
+++ KAIS.Interactive.DSPC_EXPLORER.UI/dspc-explorer-ui/src/app/screens/grave-details/grave-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grave-details',
+  templateUrl: './grave-details.component.html',
+  styleUrls: ['./grave-details.component.scss']
+})
+export class GraveDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
