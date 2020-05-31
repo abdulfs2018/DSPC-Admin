b5615baa435f0192c45be2bc110406bb03784abe
Kieron Peters <kieronpeters1@gmail.com>
2020-05-31 16:56:46 +0100

    TASK27 added changes to local storage class to remove comments out

--- KAIS.Interactive.DSPC_EXPLORER.UI/dspc-explorer-ui/src/app/core/services/dspc-explorer-provider/dspc-explorer-storage-provider.ts
+++ KAIS.Interactive.DSPC_EXPLORER.UI/dspc-explorer-ui/src/app/core/services/dspc-explorer-provider/dspc-explorer-storage-provider.ts
@@ -1,45 +1,25 @@
 import { Inject, Injectable } from '@angular/core';
 import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
 
-// key that is used to access the data in local storage
-const SEARCH_KEY = 'local_search';
-
-
 @Injectable()
 export class DSPCExplorerLocalStorageProvider {
-
-     
-     
 
      constructor(@Inject(LOCAL_STORAGE) private storage: StorageService) { }
 
 
-     public storeOnLocalStorage(key: string ,data: any): void {
-          
-          
-          //const currentTodoList = this.storage.get(SEARCH_KEY) || [];
-          
-          //const currentSearch = [];
-
-          //currentSearch.push({
-          //    data: data,
-          //});
-
-
-          // insert updated array to local storage
-          this.storage.set(SEARCH_KEY, data);
-          
+     public storeOnLocalStorage(key: string ,data: any): boolean {
+          this.storage.set(key, data);
+          return this.storage.get(key) !== undefined;
      }
 
      public getFromLocalStorage(key : string):  any {
           console.log(this.storage.get(key) || 'Local storage is empty');
-          return this.storage.get(SEARCH_KEY);
+          return this.storage.get(key);
      }
 
-    /* returns boolean, true if deleted, false if not, undefined keys also return true that it is deleted */
      public deleteFromLocalStorage(key: string): boolean {
           this.storage.remove(key);
-          return this.storage.get(key) == undefined;
+          return this.storage.get(key) === undefined;
      }
 
 }
