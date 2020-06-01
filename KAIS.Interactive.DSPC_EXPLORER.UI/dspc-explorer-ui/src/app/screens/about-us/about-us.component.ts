7e696148632ca3ce790a7140dd74bfef186fa3ff
Kieron Peters <kieronpeters1@gmail.com>
2020-06-01 20:22:46 +0100

    TASK13 added logic to show the right images on the modal when clicked on

--- KAIS.Interactive.DSPC_EXPLORER.UI/dspc-explorer-ui/src/app/screens/about-us/about-us.component.ts
+++ KAIS.Interactive.DSPC_EXPLORER.UI/dspc-explorer-ui/src/app/screens/about-us/about-us.component.ts
@@ -14,7 +14,7 @@ export class AboutUsComponent implements OnInit {
   openModal(event) {
 
     var target = event.target || event.srcElement || event.currentTarget;    
-    document.getElementById("img01").src = target.src;
+    document.getElementById("img01").setAttribute("src",target.src);
     document.getElementById("myModal").style.display = "block";
   }
 
@@ -24,34 +24,3 @@ export class AboutUsComponent implements OnInit {
 
 
 }
-
-// var AppModal = function ($scope, $modal, $log,$sce) {
-//   var parentScope = $scope;
-
-//   $scope.imgs =[];
-//   $scope.imgs.push($sce.trustAsUrl("./assets/images/graveGate.jpg"));
-//   $scope.imgs.push($sce.trustAsUrl("./assets/images/graveGate.jpg"));
-//   $scope.imgs.push($sce.trustAsUrl("./assets/images/graveGate.jpgf"));
-
-// $scope.openModalImage = function (imageSrc, imageDescription) {
-//   $modal.open({
-//     templateUrl: "../modal/modal.component.html",
-//     backdrop: true,
-//     windowClass: 'modal',
-//     resolve: {
-//         imageSrcToUse: function () {
-//             return imageSrc;
-//         },
-//         imageDescriptionToUse: function () {
-//             return imageDescription;
-//         }
-//     },
-//     controller: function ($scope, $modalInstance) {
-//       $scope.imgs = parentScope.imgs;
-//       $scope.cancel = function () {
-//           $modalInstance.dismiss('cancel');
-//       };
-//   }
-//  });
-// };
-// };
