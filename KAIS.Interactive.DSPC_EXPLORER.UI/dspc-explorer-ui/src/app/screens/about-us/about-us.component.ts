04db735a3552edacf74023eb8a38436b5bd71b37
David Asare <gohan.david11@gmail.com>
2020-06-01 13:42:59 +0100

    Added modal to images and removed red box shadow from login

--- KAIS.Interactive.DSPC_EXPLORER.UI/dspc-explorer-ui/src/app/screens/about-us/about-us.component.ts
+++ KAIS.Interactive.DSPC_EXPLORER.UI/dspc-explorer-ui/src/app/screens/about-us/about-us.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-about-us',
-  templateUrl: './about-us.component.html',
-  styleUrls: ['./about-us.component.scss']
+  selector: "app-about-us",
+  templateUrl: "./about-us.component.html",
+  styleUrls: ["./about-us.component.scss"],
 })
 export class AboutUsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  // showModalImg(e){}
+  openModal(e) {
+    console.log(e.value);
+    // document.getElementById("myImg").src;
+    document.getElementById("myModal").style.display = "block";
   }
-
+  closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
 }
+
+// var AppModal = function ($scope, $modal, $log,$sce) {
+//   var parentScope = $scope;
+
+//   $scope.imgs =[];
+//   $scope.imgs.push($sce.trustAsUrl("./assets/images/graveGate.jpg"));
+//   $scope.imgs.push($sce.trustAsUrl("./assets/images/graveGate.jpg"));
+//   $scope.imgs.push($sce.trustAsUrl("./assets/images/graveGate.jpgf"));
+
+// $scope.openModalImage = function (imageSrc, imageDescription) {
+//   $modal.open({
+//     templateUrl: "../modal/modal.component.html",
+//     backdrop: true,
+//     windowClass: 'modal',
+//     resolve: {
+//         imageSrcToUse: function () {
+//             return imageSrc;
+//         },
+//         imageDescriptionToUse: function () {
+//             return imageDescription;
+//         }
+//     },
+//     controller: function ($scope, $modalInstance) {
+//       $scope.imgs = parentScope.imgs;
+//       $scope.cancel = function () {
+//           $modalInstance.dismiss('cancel');
+//       };
+//   }
+//  });
+// };
+// };
