c54294a57f502b58072660ea1ed70de87704aa67
David Asare <gohan.david11@gmail.com>
2020-06-03 19:29:23 +0100

    Made changes to design and look of login & about us components

--- KAIS.Interactive.DSPC_EXPLORER.UI/dspc-explorer-ui/src/app/screens/home/about-us/about-us.component.ts
+++ KAIS.Interactive.DSPC_EXPLORER.UI/dspc-explorer-ui/src/app/screens/home/about-us/about-us.component.ts
@@ -10,48 +10,14 @@ export class AboutUsComponent implements OnInit {
 
   ngOnInit() {}
 
-  // showModalImg(e){}
   openModal(event) {
-
-    var target = event.target || event.srcElement || event.currentTarget;    
-    document.getElementById("img01").src = target.src;
+    var elem = document.getElementById("img01");
+    var target = event.target || event.srcElement || event.currentTarget;
+    elem.setAttribute("src", target.src);
     document.getElementById("myModal").style.display = "block";
   }
 
   closeModal() {
     document.getElementById("myModal").style.display = "none";
   }
-
-
 }
-
-// var AppModal = function ($scope, $modal, $log,$sce) {
-//   var parentScope = $scope;
-
-//   $scope.imgs =[];
-//   $scope.imgs.push($sce.trustAsUrl("./assets/images/graveGate.jpg"));
-//   $scope.imgs.push($sce.trustAsUrl("./assets/images/graveGate.jpg"));
-//   $scope.imgs.push($sce.trustAsUrl("./assets/images/graveGate.jpgf"));
-
-// $scope.openModalImage = function (imageSrc, imageDescription) {
-//   $modal.open({
-//     templateUrl: "../modal/modal.component.html",
-//     backdrop: true,
-//     windowClass: 'modal',
-//     resolve: {
-//         imageSrcToUse: function () {
-//             return imageSrc;
-//         },
-//         imageDescriptionToUse: function () {
-//             return imageDescription;
-//         }
-//     },
-//     controller: function ($scope, $modalInstance) {
-//       $scope.imgs = parentScope.imgs;
-//       $scope.cancel = function () {
-//           $modalInstance.dismiss('cancel');
-//       };
-//   }
-//  });
-// };
-// };
